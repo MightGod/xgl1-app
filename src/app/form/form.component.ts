2025385d7c2effe00de512b97f2ee08349a51cfd
uzieldavidi <uzieldavidi@gmail.com>
2019-06-05 21:59:50 +0300

    xsgl 2

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import xgl from './xgl1.json';
+import xgl2 from './xgl2.json';
 
 @Component({
   selector: 'app-form',
@@ -8,18 +9,44 @@ import xgl from './xgl1.json';
 })
 export class FormComponent implements OnInit 
 {
+  public firstTask: boolean = false;
+  public secondTask: boolean = true;
+
   public viewMode: boolean = false;
+
+  public tabPanel: any;
   public fieldPanel: any;
+
+  public activeTabIndex: number = 0;
+
   constructor() { }
 
   ngOnInit()
   {
     this.fieldPanel = xgl.wgdialog.fieldPanel;
-    console.log(this.fieldPanel.fields);
+
+    this.tabPanel = xgl2.xgl.wgdialog.tabPanel;
+    console.log(this.tabPanel.tab);
   }
 
   public submitForm(isValid : boolean) : void
   {
     console.log(`Form is ${isValid ? 'Valid' : 'Invalid'}`);
+  }
+
+  public switchTasks() : void
+  {
+    this.firstTask = !this.firstTask;
+    this.secondTask = !this.secondTask;
+  }
+
+  public isDisplayed(index: number) : boolean
+  {
+    return (this.activeTabIndex === index);
+  }
+
+  public activateTab(index: number) : void
+  {
+    this.activeTabIndex = index;
   }
 }
